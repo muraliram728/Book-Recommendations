631c27d9b2c02037637de0a17bf850d6f199497b
muraliram728 <muraliraman728@gmail.com>
2024-09-10 12:23:59 +0530

    Added Delete Product controller

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,10 +1,12 @@
 
 const express = require('express');
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController');
+const { getProducts, newProduct, getSingleProduct, UpdateProduct, DeleteProduct } = require('../controllers/productController');
 const router = express.Router();
 
 router.route('/products').get(getProducts);
 router.route('/product/new').post(newProduct);
-router.route('/product/:id').get(getSingleProduct)
+router.route('/product/:id').get(getSingleProduct);
+router.route('/product/:id').put(UpdateProduct);
+router.route('/product/:id').delete(DeleteProduct);
 
 module.exports = router;
