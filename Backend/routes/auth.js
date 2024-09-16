0a6e9fcfdfc095cb1f61c3a0753abb2b558ca45b
muraliram728 <muraliraman728@gmail.com>
2024-09-16 19:51:34 +0530

    get user profile

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { registerUser, loginUser, logoutUser, forgotPassword } = require('../controllers/authController');
+const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile } = require('../controllers/authController');
 const router = express.Router();
+const {isAuthendicatedUser} = require('../middlewares/authendicate');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logoutUser);
 router.route('/password/forgot').post(forgotPassword);
-
+router.route('/password/reset/:token').post(resetPassword);
+router.route('/myprofile').get(isAuthendicatedUser,getUserProfile);
 module.exports = router;
