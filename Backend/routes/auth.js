d59aad3c2b9cafdd025682014c1befbc06f8a054
muraliram728 <muraliraman728@gmail.com>
2024-09-17 07:55:25 +0530

    added chabge password

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile } = require('../controllers/authController');
+const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile, changePassword } = require('../controllers/authController');
 const router = express.Router();
 const {isAuthendicatedUser} = require('../middlewares/authendicate');
 
@@ -9,4 +9,5 @@ router.route('/logout').get(logoutUser);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').post(resetPassword);
 router.route('/myprofile').get(isAuthendicatedUser,getUserProfile);
+router.route('/password/change').put(isAuthendicatedUser,changePassword);
 module.exports = router;
