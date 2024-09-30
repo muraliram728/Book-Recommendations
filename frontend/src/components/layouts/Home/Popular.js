98309c07f74320ecccb1fb00573722009a81db35
muraliram728 <muraliraman728@gmail.com>
2024-09-30 17:30:39 +0530

    Updated Home and Popular components; deleted Frontend directory

--- frontend/src/components/layouts/Home/Popular.js
+++ frontend/src/components/layouts/Home/Popular.js
@@ -1,35 +1,33 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
-import { Button, Container, Paper } from '@mui/material';
-
-
-
+import { Paper } from '@mui/material';
+import './Popular.css'; // Import the CSS file
 
 export default function Popular() {
   return (
     <Box >
-      <h4>Popular Groups</h4>
+      <h4 className="popular-title">Popular Groups</h4>
       <Grid container spacing={2}>
-        <Grid size={4} >
-          <Paper sx={{ minHeight: "219px", minWidth: "160px" }}>size=8</Paper>
+        <Grid size={4}>
+          <Paper className="Paper-1">size=8</Paper>
         </Grid>
-        <Grid size={8} >
-          <Paper sx={{ minHeight: '100px', minWidth: '470px' }}>size=4</Paper>
-          <Box display={'flex'} mt={2} gap={2}>
-            <Grid size={8}  >
-              <Paper sx={{ minHeight: '100px', minWidth: '320px' }}>hhhehheh</Paper>
+        <Grid size={8}>
+          <Paper className="paper-extra-large">size=4</Paper>
+          <Box className="flex-box">
+            <Grid size={8}>
+              <Paper className="paper-medium">hhhehheh</Paper>
             </Grid>
-            <Grid size={4} >
-              <Paper sx={{ minHeight: '100px', minWidth: "140px" }}>ioehqw </Paper>
+            <Grid size={4}>
+              <Paper className="paper-small">ioehqw</Paper>
             </Grid>
           </Box>
         </Grid>
-        <Grid size={4} display={'flex'} gap={2}>
-          <Paper sx={{ minHeight: '100px', minWidth: '350px' }}>size=8</Paper>
-          <Paper sx={{ minHeight: '100px', minWidth: '356px' }}>size=8</Paper>
+        <Grid size={4} className="flex-box2">
+          <Paper className="paper-medium-large">size=8</Paper>
+          <Paper className="paper-medium-large-alt">size=8</Paper>
         </Grid>
       </Grid>
     </Box>
-  )
+  );
 }
